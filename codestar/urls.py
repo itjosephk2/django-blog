21e72bbe4e0978aed05cf3f264f92f8ad5fe1d30
Joseph Keane <josephkeane996@gmail.com>
2024-08-22 14:16:49 +0000

    added home page

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import my_blog
+from blog.views import my_blog, home_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('blog/', my_blog, name='blog'),
 ]
