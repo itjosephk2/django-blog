3ed241ec95603f276b97ccd70bd0d366b9fe0187
Joseph Keane <josephkeane996@gmail.com>
2024-09-13 13:51:58 +0000

    comment model

--- blog/models.py
+++ blog/models.py
@@ -11,4 +11,12 @@ class Post(models.Model):
     content = models.TextField
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    updated_on = models.DateTimeField(auto_now=True)
+    updated_o
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
